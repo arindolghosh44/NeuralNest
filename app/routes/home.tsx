09cc625537b45d0f123738e2a020a6dba5333db2
Arindol Ghosh <144908811+arindolghosh44@users.noreply.github.com>
2026-02-22 19:10:44 +0530

    update static home

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,7 +2,7 @@ import type { Route } from "./+types/home";
 import Navbar from "../../components/Navbar";
 import {ArrowRight, ArrowUpRight, Clock, Layers} from "lucide-react";
 import Button from "../../components/ui/Button";
-
+import Upload from "../../components/Upload";
 import {useNavigate} from "react-router";
 import {useEffect, useRef, useState} from "react";
 
@@ -19,7 +19,6 @@ export default function Home() {
     const [projects, setProjects] = useState<DesignItem[]>([]);
     const isCreatingProjectRef = useRef(false);
 
-
     return (
         <div className="home">
             <Navbar />
@@ -33,7 +32,7 @@ export default function Home() {
                     <p>Introducing NeuralNest 2.0</p>
                 </div>
 
-                <h1>✨ Imagine it, design it, and watch it come alive instantly with NeuralNest.</h1>
+                <h1>Build beautiful spaces at the speed of thought with NeuralNest</h1>
 
                 <p className="subtitle">
                     NeuralNest is an AI-first design environment that helps you visualize, render, and ship architectural projects faster  than ever.
@@ -62,7 +61,7 @@ export default function Home() {
                             <p>Supports JPG, PNG, formats up to 10MB</p>
                         </div>
 
-                    <p>Upload Image</p>
+                        <Upload  />
                     </div>
                 </div>
             </section>
@@ -72,17 +71,16 @@ export default function Home() {
                     <div className="section-head">
                         <div className="copy">
                             <h2>Projects</h2>
-                            <p>🌟 Explore your latest creations and dive into inspiring community projects — all together in one vibrant space.</p>
+                            <p>Your latest work and shared community projects, all in one place.</p>
                         </div>
                     </div>
 
                     <div className="projects-grid">
-                        {projects.map(({id, name, renderedImage, sourceImage, timestamp}) => (
-                            <div key={id} className="project-card group" onClick={() => navigate(`/visualizer/${id}`)}>
-                                <div className="preview">
-                                    <img  src={renderedImage || sourceImage} alt="Project"
-                                    />
 
+                            <div  className="project-card group" >
+                                <div className="preview">
+                                    <img  src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png" alt="Project"
+                                    />
 
                                     <div className="badge">
                                         <span>Community</span>
@@ -91,12 +89,12 @@ export default function Home() {
 
                                 <div className="card-body">
                                     <div>
-                                        <h3>{name}</h3>
+                                        <h3>Manhatten</h3>
 
                                         <div className="meta">
                                             <Clock size={12} />
-                                            <span>{new Date(timestamp).toLocaleDateString()}</span>
-                                            <span>By GROUP - B 6TH SEM</span>
+                                            <span>{new Date('01.01.2027').toLocaleDateString()}</span>
+                                            <span>By GROUP-B 6th SEM</span>
                                         </div>
                                     </div>
                                     <div className="arrow">
@@ -104,7 +102,7 @@ export default function Home() {
                                     </div>
                                 </div>
                             </div>
-                        ))}
+
                     </div>
                 </div>
             </section>
